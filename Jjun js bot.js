28597894e22d2cb9a761ce86478c12b4e5dde3b0
DESKTOP-1Q06HC2\docto <doctoralex@naver.com>
2020-12-14 17:10:29 +0900

    Main Sctipt

---
+++ Jjun js bot.js
@@ -0,0 +1,20 @@
+const scriptName="Jjun bot.js";
+
+function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId){
+    /*(이 내용은 길잡이일 뿐이니 지우셔도 무방합니다)
+     *(String) room: 메시지를 받은 방 이름
+     *(String) msg: 메시지 내용
+     *(String) sender: 전송자 닉네임
+     *(boolean) isGroupChat: 단체/오픈채팅 여부
+     *replier: 응답용 객체. replier.reply("메시지") 또는 replier.reply("방이름","메시지")로 전송
+     *(String) ImageDB.getProfileImage(): 전송자의 프로필 이미지를 Base64로 인코딩하여 반환
+     *(String) packageName: 메시지를 받은 메신저의 패키지 이름. (카카오톡: com.kakao.talk, 페메: com.facebook.orca, 라인: jp.naver.line.android
+     *(int) threadId: 현재 쓰레드의 순번(스크립트별로 따로 매김)     *Api,Utils객체에 대해서는 설정의 도움말 참조*/
+    
+}
+
+function onStartCompile(){}
+function onCreate(savedInstanceState,activity) {}
+function onResume(activity) {}
+function onPause(activity) {}
+function onStop(activity) {}
